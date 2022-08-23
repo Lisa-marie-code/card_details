2ffc9f1bfd18fe7db1aa6423856ad34a7ab099b0
Lisa-marie-code <lisamariekoomson340@gmail.com>
2022-08-23 12:37:06 +0000

    card details updated

---
+++ index.js
@@ -0,0 +1,36 @@
+
+function validateNum() {
+  let numDetails = document.getElementById("number_details").value;
+  let carding =  /^\d{16}$/;
+
+  if (carding.test(numDetails)) {
+    document.getElementById("num_error").style.visibility = "hidden";
+    
+    console.log(numDetails);
+  } else {
+    document.getElementById("num_error").style.visibility = "visible";
+    document.getElementsByName("number-exact")[0].style.borderColor = "red";
+  }
+  }
+
+
+document.getElementById('holder_details').oninput = () =>{
+  document.getElementById('card_name_new').innerText = document.getElementById('holder_details').value;
+}
+
+document.getElementById('number_details').oninput = () =>{
+  document.getElementById('card_number_new').innerText = document.getElementById('number_details').value;
+}
+
+document.getElementById('exp_month').oninput = () =>{
+  document.getElementById('month_span').innerText = document.getElementById('exp_month').value;
+}
+
+document.getElementById('exp_year').oninput = () =>{
+  document.getElementById('year_span').innerText = document.getElementById('exp_year').value;
+}
+
+document.getElementById('exp_cvc').oninput = () =>{
+  document.getElementById('cvc_newInput').innerText = document.getElementById('exp_cvc').value;
+}
+
